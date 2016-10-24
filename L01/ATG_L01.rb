3c61bcca0c5af95366351a3e35b812f8c4a37e05
Marek Grabowski <Marek.Grabowski@sapiens.com>
2016-10-24 16:57:35 +0200

    [L01] Fixes

--- L01/ATG_L01.rb
+++ L01/ATG_L01.rb
@@ -4,13 +4,17 @@
 
 require 'matrix'
 
+DEBUG = false
+
 class Matrix
   def []=(row, column, value)
     @rows[row][column] = value
   end
 end
 
-$matrix = Matrix.empty(0,0)
+def putsd(string)
+  puts string if DEBUG
+end
 
 # zarzadzanie wierzcholkami
 def addVertex(matrix)
@@ -28,14 +32,15 @@ def addVertex(matrix)
   return matrix
 end
 
-def remVertex(index)
-  arr = $matrix.transpose.to_a
+def remVertex(matrix, index)
+  arr = matrix.transpose.to_a
   arr.each do |row|
     row.delete_at index
   end
   arr.delete_at index
-  $matrix = Matrix.rows(arr)
-  $matrix.normal? #sprawdzenie czy macierz jest poprawnego rozmiaru
+  matrix = Matrix.rows(arr)
+  matrix.normal? #sprawdzenie czy macierz jest poprawnego rozmiaru
+  return matrix
 end
 
 # zarzadzanie krawedziami
@@ -46,31 +51,27 @@ def addEdge(matrix, row, col)
   return matrix
 end
 
-def remEdge(row, col)
-  $matrix.[]=(row, col, 0)
-  $matrix.[]=(col, row, 0)
+def remEdge(matrix, row, col)
+  matrix.[]=(row, col, 0)
+  matrix.[]=(col, row, 0)
+  return matrix
 end
 
-
-
-def nextIndex
-  return $matrix.row_size + 1
+def nextIndex(matrix)
+  return matrix.row_size + 1
 end
 
 def printMatrix(matrix)
-  # TODO: stworzyc ladne rysowanie macierzy w konsoli
-  i = matrix.row_size
-  j = matrix.column_size
-  puts "----------"
+  # TODO: stworzyc ladne rysowanie grafu w konsoli
   matrix.to_a.each do |row|
     puts row.to_s
   end
 end
 
-def showStats
+def showStats(matrix)
   # wyznaczenie stopnia wierzcholka oraz minimalnego i maksymalnego stopnia grafu, wyznaczenie, ile jest wierzcholkow stopnia parzystego i nieparzystego
   ranks = Array.new
-  $matrix.to_a.each do |row|
+  matrix.to_a.each do |row|
     rank = 0
     row.each do |i|
       rank += i
@@ -98,7 +99,7 @@ end
 
 # 1.2 W oparciu o reprezentacje grafu w postaci macierzy sasiedztwa zaimplementuj procedure, ktora sprawdza, czy graf zawiera podgraf izomorficzny do cyklu C3.
 def c3naive?(matrix)
-  arr = Array.new($matrix.row_size){ |index| index + 1}
+  arr = Array.new(matrix.row_size){ |index| index + 1}
   arr.permutation(3).each do |e|
     return true if matrix.element(e[0],e[1]) == 1 && matrix.element(e[0],e[2]) == 1 && matrix.element(e[1],e[2]) == 1
   end
@@ -114,7 +115,6 @@ end
 # 1.3a Zaimplementuj procedure sprawdzajaca, czy dany (nierosnacy) ciag liczb naturalnych jest ciagiem grafowym.
 def graphSequence(sequence)
   sequence.sort! {|x,y| y <=> x }
-  puts "mlem: #{sequence.to_s}"
   return true if sequence[0] <= 0
   1.upto(sequence[0]) do |i|
     sequence[i] = sequence[i] - 1
@@ -128,55 +128,55 @@ end
 
 #1.3b Zaimplementuj procedure, ktora w przypadku odpowiedzi pozytywnej na punkt (a) zwroci (jakikolwiek) graf prosty (w postaci macierzy sasiedztwa) realizujacy ten ciag.
 def simpleGraphBySequence(sequence)
-  #przygotowanie pustej macierzy
-  matrix = Matrix.empty(0,0)
-  1.upto(sequence.length) {matrix = addVertex(matrix)}
-  #sequence.sort! {|x,y| y <=> x }
-
-  1.upto(sequence.length) do
-    id = sequence.index(sequence.max)
-    puts "#{sequence} #{id} #{sequence.max}"
-    1.upto(sequence[id]) do |mv|
-
-      if (id + mv + 1) <= sequence.length
-        puts "#id: #{id+1}, mv: #{mv}, idmv: #{id+mv+1} ,sl: #{sequence.length}"
-        matrix = addEdge(matrix, id+1, id + mv + 1)
-        sequence[id] -= 1
-        sequence[id+mv] -= 1
-      else
-        puts "#id: #{id+1}, mv: #{mv}, idmv: #{id+mv+1} ,sl: #{sequence.length}"
-        mv = sequence.length - (mv + id+1)
-        puts "AOI - new mv: #{mv}, idmv: #{id+mv+1} "
-        matrix = addEdge(matrix, id+1, id + mv+1)
-        sequence[id] -= 1
-        sequence[id+mv] -= 1
+  if graphSequence Array.new(sequence)
+    #przygotowanie pustej macierzy
+    matrix = Matrix.empty(0,0)
+    1.upto(sequence.length) {matrix = addVertex(matrix)}
+    1.upto(sequence.length) do
+      id = sequence.index(sequence.max)
+      putsd "#{sequence} #{id} #{sequence.max}"
+      1.upto(sequence[id]) do |mv|
+        if (id + mv + 1) <= sequence.length
+          putsd "#id: #{id+1}, mv: #{mv}, idmv: #{id+mv+1} ,sl: #{sequence.length}"
+          matrix = addEdge(matrix, id+1, id + mv + 1)
+          sequence[id] -= 1
+          sequence[id+mv] -= 1
+        else
+          putsd "#id: #{id+1}, mv: #{mv}, idmv: #{id+mv+1} ,sl: #{sequence.length}"
+          mv = sequence.length - (mv + id+1)
+          putsd "AOI - new mv: #{mv}, idmv: #{id+mv+1} "
+          matrix = addEdge(matrix, id+1, id + mv+1)
+          sequence[id] -= 1
+          sequence[id+mv] -= 1
+        end
       end
     end
+    puts "Podany ciag reprezentuje graf w postaci macierzy sasiedztwa:"
+    printMatrix(matrix)
+    return matrix
+  else
+    puts "Podany ciag nie jest reprezentacja grafu prostego."
+    return null
   end
-  printMatrix(matrix)
 end
 
-
 # MAIN
-#addVertex($matrix)
-#addVertex($matrix)
-#addVertex($matrix)
-#addVertex($matrix)
-#$matrix = addEdge($matrix,1,2)
-#$matrix = addEdge($matrix,2,3)
-#$matrix = addEdge($matrix,3,4)
-#$matrix = addEdge($matrix,1,4) #
-#addEdge(1,3)
-
-
-#addEdge(2,4)
-#printMatrix($matrix)
-#showStats
-#puts "------ tst -------"
-#puts c3naive?($matrix)
-#puts c3multiply?($matrix)
-
-#puts graphSequence([3,3,3,3])
-
-
-simpleGraphBySequence([3,3,3,3])
+puts "--- 1.1 a"
+matrix = Matrix.empty(0,0)
+matrix = addVertex(matrix)
+matrix = addVertex(matrix)
+matrix = addVertex(matrix)
+matrix = addVertex(matrix)
+matrix = addEdge(matrix,1,2)
+matrix = addEdge(matrix,2,3)
+matrix = addEdge(matrix,3,4)
+matrix = addEdge(matrix,1,4)
+printMatrix(matrix)
+puts "--- 1.1 b-d"
+showStats(matrix)
+puts "--- 1.2 a"
+puts c3naive?(matrix)
+puts "--- 1.2 b"
+puts c3multiply?(matrix)
+puts "--- 1.3 a-b"
+simpleGraphBySequence([3,3,2,2])
