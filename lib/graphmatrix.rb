bd23ca2a6704c85c2da416ddc7ab1e019df09aa9
Marek Grabowski(Grabarzstg) <ereporganizacja@gmail.com>
2016-10-24 23:32:02 +0200

    [LIB] Move part of code from L01/ATG_L01.rb to lib/graphmatrix.rb

--- lib/graphmatrix.rb
+++ lib/graphmatrix.rb
@@ -1,6 +1,77 @@
-#require '../lib/graphmatrix'
-module GraphMatrix
-  class GraphMatrix
-    extend Matrix
+require 'matrix'
+class GraphMatrix < Matrix
+  def []=(row, column, value)
+    @rows[row][column] = value
+  end
+
+  #tools
+  def print
+    # TODO: create simple graph drawing in console
+    @rows.each do |row|
+      puts row.to_s
+    end
+  end
+
+  # vertex management
+  def addVertex
+    row = Array.new
+    (0..self.row_size).each do
+      row << 0
+    end
+    @rows.each do |r|
+      r << 0
+    end
+    @rows << row
+    self.square?
+  end
+
+  def remVertex(index)
+    arr = self.transpose.to_a
+    arr.each do |row|
+      row.delete_at index
+    end
+    arr.delete_at index
+    @rows = arr
+    self.square?
+  end
+
+  # edges management
+  def addEdge(row, col)
+    raise IndexError if row > self.row_size || row < 0 || col > self.row_size || col < 0
+    @rows[row-1][col-1] = 1
+    @rows[col-1][row-1] = 1
+  end
+
+  def remEdge(row, col)
+    self.[]=(row, col, 0)
+    self.[]=(col, row, 0)
+  end
+
+  def showStats
+    ranks = Array.new
+    self.to_a.each do |row|
+      rank = 0
+      row.each do |i|
+        rank += i
+      end
+      ranks << rank
+    end
+    even = 0
+    odd = 0
+    ranks.each do |e|
+      if e.even?
+        even += 1
+      else
+        odd += 1
+      end
+    end
+
+    puts "----- STATS -----"
+    puts "Ranks: #{ranks.to_s}"
+    puts "Ranks (sorted): #{ranks.sort {|x,y| y <=> x }}"
+    puts "Min rank: #{ranks.min}"
+    puts "Max rank: #{ranks.max}"
+    puts "Odd ranks: #{odd}"
+    puts "Even ranks: #{even}"
   end
 end
