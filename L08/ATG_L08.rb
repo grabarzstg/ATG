88e7531549636ce916f118fb091eb36d20f965b8
Marek Grabowski(Grabarzstg) <ereporganizacja@gmail.com>
2017-01-04 20:54:07 +0100

    [L08] Edmons-Karp

--- L08/ATG_L08.rb
+++ L08/ATG_L08.rb
@@ -9,21 +9,39 @@ end
 
 
 
+
 def EdmonsKarp(c, t, source, target)
   flow = 0        #value of maximum flow
-  f = Hash.new   #flow matrix
+  f = Hash.new(0)   #flow matrix
+  # stworzenie sieci residualnej
+  r = Hash.new
+  f.each do |k, v|
+    r[k] = c[k] - f[k]
+  end
+  path = true
+  while path do
+    #znalezienie ścieżki z s do t w sieci residualnej
+    p = BFS(r, source, target)
+    if p.nil?
+      path = false
+    else
+      # powiekszenie przeplywu na sciezce p
+      #a = min(r[u,v])
+      puts "ELSE"
+    end
+  end
+end
 
+def BFS(r, s, t)
+  r.each do |key, val|
 
-
+  end
 
 end
 
-
-
-
-
 #INPUT
 c = Hash.new   #capacity matrix
+g =
 source = ""
 target = ""
 File.readlines("input.csv").each_with_index do |line, index|
@@ -31,7 +49,7 @@ File.readlines("input.csv").each_with_index do |line, index|
     next
   end
   tmp = line.split(",")
-  c["#{tmp[0]}#{tmp[1]}"] = tmp[2]
+  c["#{tmp[0]}#{tmp[1]}"] = tmp[2].to_i
   source = tmp[0] if tmp[3]
   target = tmp[1] if tmp[4]
 end
